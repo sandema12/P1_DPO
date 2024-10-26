a1300ff834b2f5e78d142d349f174d26cc0cc61a
sandema12 <157759576+sandema12@users.noreply.github.com>
2024-10-25 22:40:45 -0500

    Delete P1-2/LearningPath directory

--- P1-2/LearningPath/src/Consola/Principal.java
+++
@@ -1,8 +0,0 @@
-package Consola;
-
-public class Principal {
-    public static void main(String[] args) {
-        ConsolaAutenticacion consola = new ConsolaAutenticacion();
-        consola.mostrarMenu();
-    }
-}
